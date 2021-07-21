bf659ae154ffd1f05e30e704528befe495848b66
Mariane <nani_karoline@hotmail.com>
2021-07-21 17:29:03 -0300

    Programa cadastro em banco

---
+++ ExercicioBanco/ContaBancaria.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace ExercicioBanco
+{
+    class ContaBancaria
+    {
+        public int NumeroConta { get; set; }
+        public string Nome { get; set; }
+        public double Saldo { get; set; }
+
+        public ContaBancaria(int num, string nome, double deposito)
+        {
+            NumeroConta = num;
+            Nome = nome;
+            Saldo = deposito;
+        }
+
+        public void Depositar(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Sacar(double saque)
+        {
+            Saldo = Saldo - saque - 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "\nNuméro da conta: " + NumeroConta 
+                + "\nPessoa Física: " + Nome 
+                + "\nSaldo: $" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExercicioBanco/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opc = 1;
+            double deposito = 0.0;
+            Console.Write("Digite o número da conta bancária: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Deseja fazer um depósito inicial? (S/N) ");
+            char op = char.Parse(Console.ReadLine().ToUpper());
+
+            switch (op)
+            {
+                case 'S':
+                    Console.Write("Por favor, digite o valor a ser depositado: $");
+                    deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    break;
+                case 'N':
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+            ContaBancaria cb = new ContaBancaria(numeroConta, nome, deposito);
+
+            Console.WriteLine(cb);
+
+            while (opc != 0)
+            {
+                Console.WriteLine("\n-------------------------------------\n");
+                Console.WriteLine("Menu");
+                Console.WriteLine("1 - Depositar");
+                Console.WriteLine("2 - Sacar");
+                Console.WriteLine("0 - Sair");
+                Console.Write("Opção: ");
+                opc = int.Parse(Console.ReadLine());
+
+                switch (opc)
+                {
+                    case 1:
+                        Console.Write("\nDigite o valor a ser depositado: $");
+                        cb.Depositar(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+                        break;
+                    case 2:
+                        Console.Write("\nDigite o valor a ser sacado: $");
+                        cb.Sacar(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("\nOpção inválida, tente novamente");
+                        continue;
+                }
+                Console.WriteLine("\n-------------------------------------\n");
+                Console.WriteLine(cb);
+
+            }
+
+        }
+    }
+}
